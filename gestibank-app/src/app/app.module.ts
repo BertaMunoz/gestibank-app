fa663672789dab0d45bd272658af7602851c5ac0
noelle chaffurin <noelle.chaffurin@gmail.com>
2019-04-25 10:24:08 +0200

    creation component compte + form creation compte + final page index

--- gestibank-app/src/app/app.module.ts
+++ gestibank-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateCompteComponent } from './create-compte/create-compte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCompteComponent
   ],
   imports: [
     BrowserModule,
