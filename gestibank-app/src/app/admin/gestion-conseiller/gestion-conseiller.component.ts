2631a1b7b22dfea845bcd39df837bd7ed71d2cb3
Adel ESSAFI <adelessafi@gmail.com>
2019-04-25 15:28:44 +0200

    variable statique

--- gestibank-app/src/app/admin/gestion-conseiller/gestion-conseiller.component.ts
+++ gestibank-app/src/app/admin/gestion-conseiller/gestion-conseiller.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {  ConseillerService } from '../../conseiller/conseiller.service';
+import {Conseiller} from '../../model/conseiller';
 
 @Component({
   selector: 'app-gestion-conseiller',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionConseillerComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  listConseillers = [
+    new Conseiller(0,"24/04/2019","TARDIOU","kim","tk@gmail.com","0700000000","5 rue de la fontaine",69000,"Lyon"),
+    new Conseiller(1,"12/03/2019","stephane","stephane","stephane@gmail.com","0711111111","10 rue jean jaures",69000,"Lyon")
+  ];
+  constructor( ) {
   }
+
+  ngOnInit() {}
+
 
 }
